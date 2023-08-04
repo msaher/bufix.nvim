fe2c74b53a99b3c3fcd3519fcf515325c2d91a12
saher <msaher.shair@gmail.com>
2023-08-04 12:51:36 +0300

    feat: add main Compile logic

--- lua/compile.lua
+++ lua/compile.lua
@@ -36,3 +36,52 @@ function Compile:_has_buf()
     -- return self.buf ~= nil
     return fn.bufexists(self.buf) ~= 0
 end
+
+function Compile:_rest()
+
+    if self.job ~= nil then
+        fn.jobstop(self.job)
+        self.job = nil
+    end
+end
+
+function Compile:_execute()
+    if not self:_has_buf() then
+        self.buf = A.nvim_create_buf(true, true)
+    end
+
+    -- TODO: make the window opening function dynamic
+    local win = self:get_win()
+    local win_curr = A.nvim_get_current_win()
+    if win == nil then
+        vim.cmd.split()
+        vim.cmd.wincmd('J')
+        win = A.nvim_get_current_win()
+    end
+
+    A.nvim_win_set_buf(win, self.buf)
+
+    local buf_curr = A.nvim_get_current_buf()
+    A.nvim_set_current_buf(self.buf)
+    vim.opt_local.modified = false
+    self.job = fn.termopen(self.cmd, {
+        detach = false,
+        cwd = fn.getcwd(),
+        stderr_buffered = true,
+        stdout_buffered = true,
+    })
+
+    A.nvim_buf_set_name(self.buf, '*compile*: ' .. table.concat(self.cmd, " "))
+
+    -- go back
+    A.nvim_set_current_buf(buf_curr)
+    A.nvim_set_current_win(win_curr)
+
+end
+
+function Compile:start()
+    self:_rest()
+    self:_execute()
+end
+
+return Compile
