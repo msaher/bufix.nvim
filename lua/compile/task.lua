ecc018cd6ca744d48d0fab6ce6c1b15bc3af0b14
saher <msaher.shair@gmail.com>
2024-01-09 23:04:24 +0300

    feat: allow dymaically renaming a task

--- lua/compile/task.lua
+++ lua/compile/task.lua
@@ -88,6 +88,7 @@ function Task:_termopen()
         cwd = self.opts.cwd,
         stderr_buffered = true,
         stdout_buffered = true,
+
         on_exit = function(job_id, exit_code, event)
             if self.opts.on_exit ~= nil then
                 self.opts.on_exit(self, job_id, exit_code, event)
@@ -134,14 +135,8 @@ function Task:_execute()
     local buf_curr = A.nvim_get_current_buf()
     self:_termopen()
 
-    local name
-    if type(self.opts.cmd) == 'table' then
-        name = table.concat(self.opts.cmd, " ")
-    else
-        name = self.opts.cmd
-    end
-
-    A.nvim_buf_set_name(self.buf, '*task*: ' .. name)
+    A.nvim_buf_set_name(self.buf, self:get_name())
+    self:set_buf_name()
 
     -- go back to original buffer
     A.nvim_set_current_buf(buf_curr)
@@ -178,6 +173,30 @@ function Task:die()
     if self:has_buf() then
         A.nvim_buf_delete(self.buf, {force = true})
         self.buf = nil
+    end
+end
+
+function Task:get_name()
+    local name = self.opts.name
+    if name ~= nil then
+        return name
+    elseif type(self.opts.cmd) == 'table' then
+        name = "*task*: " .. table.concat(self.opts.cmd, " ")
+    else
+        name = "*task*: " .. self.opts.cmd
+    end
+
+    return name
+end
+
+function Task:set_name(name)
+    self.opts.name = name
+end
+
+function Task:set_buf_name()
+    local buf = self:get_buf()
+    if buf ~= nil then
+        A.nvim_buf_set_name(self.buf, self:get_name())
     end
 end
 
