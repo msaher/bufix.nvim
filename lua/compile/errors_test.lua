a84f2d454674ffcd6d0d3a51cd94a2f77589c892
saher <msaher.shair@gmail.com>
2024-10-27 17:03:48 +0300

    test: add pattern tests

---
+++ lua/compile/errors_test.lua
@@ -0,0 +1,55 @@
+local errors = require("compile.errors")
+local busted = require("plenary.busted")
+
+
+local function tbl_equal(a, b)
+    if a == b then return true end
+
+    if type(a) ~= "table" or type(b) ~= "table" then
+        return false
+    end
+
+    for k, v in pairs(a) do
+        if type(v) == "table" then
+            if not tbl_equal(v, b[k]) then
+                return false
+            end
+        else
+            if v ~= b[k] then
+                return false
+            end
+        end
+    end
+
+    -- make "a" has all keys "b" has
+    for k in pairs(b) do
+        if a[k] == nil then
+            return false
+        end
+    end
+
+    return true
+end
+
+
+busted.describe("error patterns", function()
+
+    local cases = {
+        { rule = "absoft", line = "Error on line 3 of t.f: Execution error unclassifiable statement", want = {row_start = 3, filename = "t.f" }},
+        { rule = "absoft", line = "Line 45 of \"foo.c\": bloofle undefined", want = {row_start = 45, filename = "foo.c" }},
+        { rule = "absoft", line = "error on line 19 of fplot.f: spelling error?", want = {row_start = 19, filename = "fplot.f" }},
+        { rule = "absoft", line = "warning on line 17 of fplot.f: data type is undefined for variable d", want = {row_start = 17, filename = "fplot.f" }},
+
+    }
+
+    busted.it("captures error information", function()
+        for i, v in ipairs(cases) do
+            local got = errors.patterns[v.rule]:match(v.line)
+            assert(
+                tbl_equal(got, v.want),
+                string.format("Test #%d failed for rule '%s' with line '%s'\n\tgot %s, want %s", i, v.rule, v.line, vim.inspect(got), vim.inspect(v.want))
+            )
+        end
+    end)
+end)
+
