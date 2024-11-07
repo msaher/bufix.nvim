9e445781a9b3329d56cb3c9ac9828d00e34b7f9e
saher <msaher.shair@gmail.com>
2024-11-07 13:25:23 +0300

    chore: remove main call

--- lua/compile/task.lua
+++ lua/compile/task.lua
@@ -219,29 +219,4 @@ function Task:run(cmd, opts)
     errors.set_cwd(cwd)
 end
 
-local function main()
-    -- clean *Task* if it exists
-    local buf = vim.iter(vim.api.nvim_list_bufs()):find(function(b)
-        return vim.fs.basename(vim.api.nvim_buf_get_name(b)) == "*Task*"
-    end)
-
-    if buf ~= nil then
-        vim.api.nvim_buf_delete(buf, { force = true })
-    end
-
-    local t = Task.new("*Task*")
-    vim.api.nvim_buf_create_user_command(0, "T", function()
-        t:run("ls")
-    end, { force = true })
-
-    local last_input = nil
-    vim.keymap.set("n", "<leader>r", function()
-        input = vim.fn.input({ prompt = "Command to run: ", default = last_input or ""})
-        if input ~= nil and input ~= "" then
-            t:run(input)
-            last_input = input
-        end
-    end)
-end
-
-main()
+return Task
