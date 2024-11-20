a3960d9216b53f54b9f16f352dc3a96f5c5d45a7
saher <msaher.shair@gmail.com>
2024-11-20 07:35:05 +0300

    refactor: move default task to task module

--- lua/doit/init.lua
+++ lua/doit/init.lua
@@ -1,30 +1,6 @@
 local M = {}
 
-M.task = require("doit.task").new() -- default task. Covers 99% of use cases
-
--- completion function for prompt_for_cmd()
--- credit goes to https://github.com/ej-shafran/compile-mode.nvim
-vim.cmd[[
-function! DoitInputComplete(ArgLead, CmdLine, CursorPos)
-    let HasNoSpaces = a:CmdLine =~ '^\S\+$'
-    let Results = getcompletion('!' . a:CmdLine, 'cmdline')
-    let TransformedResults = map(Results, 'HasNoSpaces ? v:val : a:CmdLine[:strridx(a:CmdLine, " ") - 1] . " " . v:val')
-    return TransformedResults
-endfunction
-]]
-
-
----@param opts RunOpts?
-function M.prompt_for_cmd(opts)
-    local input = vim.fn.input({
-        prompt = "Command to run: ",
-        default = M.task.last_cmd or "",
-        completion = "customlist,DoitInputComplete",
-    })
-
-    if input ~= nil and input ~= "" then
-        M.task:run(input, opts)
-    end
+function M.setup()
 end
 
 return M
--- lua/doit/task.lua
+++ lua/doit/task.lua
@@ -268,4 +268,52 @@ do
     vim.api.nvim_set_hl(0, "DoitTaskAbnormal", { link = "WarningMsg", default = true })
 end
 
-return Task
+-- completion function for prompt_for_cmd()
+-- credit goes to https://github.com/ej-shafran/compile-mode.nvim
+-- Thank you!
+vim.cmd[[
+function! DoitInputComplete(ArgLead, CmdLine, CursorPos)
+    let HasNoSpaces = a:CmdLine =~ '^\S\+$'
+    let Results = getcompletion('!' . a:CmdLine, 'cmdline')
+    let TransformedResults = map(Results, 'HasNoSpaces ? v:val : a:CmdLine[:strridx(a:CmdLine, " ") - 1] . " " . v:val')
+    return TransformedResults
+endfunction
+]]
+
+vim.cmd[[
+function! DoitCliComplete(ArgLead, CmdLine, CursorPos)
+    let HasNoSpaces = a:CmdLine =~ '^\S\+$'
+    let Results = getcompletion('!' . a:CmdLine, 'cmdline')
+    let TransformedResults = map(Results, 'HasNoSpaces ? v:val : a:CmdLine[:strridx(a:CmdLine, " ") - 1] . " " . v:val')
+    return TransformedResults
+endfunction
+]]
+
+---@param opts RunOpts?
+function Task:prompt_for_cmd(opts)
+    local input = vim.fn.input({
+        prompt = "Command to run: ",
+        default = self.last_cmd or "",
+        completion = "customlist,DoitInputComplete",
+    })
+
+    if input ~= nil and input ~= "" then
+        self:run(input, opts)
+    end
+
+    -- vim.ui.input({
+    --     prompt = "Command to run: ",
+    --     default = self.last_cmd or "",
+    --     completion = "customlist,DoitInputComplete",
+    -- }, function(input)
+    --         if input ~= nil and input ~= "" then
+    --             self:run(input, opts)
+    --         end
+    --     end
+    -- )
+
+end
+
+local task = Task.new()
+
+return task
